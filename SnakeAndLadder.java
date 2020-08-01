05f90ce217d6fba760c0749910acaa0de8be0acb
shashank srivastava <shashank.1696@gmail.com>
2020-08-01 15:22:48 +0530

    ADDED: Used Random to roll the dice between 1 to 6

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -2,18 +2,22 @@ import java.util.Random;
 
 public class SnakeAndLadder {
 	
-	//Initializing
-	//CONSTANT 
+	//Initializing Constant and Variable 
 	int START_POSITION = 0;
-	int NO_OF_PLAYER = 1;
+	int noOfPlayer = 1;
 
 	public static void main(String[] args) {
 	
-		//Here the dice is rolled to get the value in between 1 to 6
+		SnakeAndLadder sl = new SnakeAndLadder();
+		sl.rollDice();	
+	}
+	
+	// Here player rolls a dice to get the number between 1 to 6
+	private int rollDice() {
 		Random random = new Random();
 		int diceRolled = random.nextInt(6) + 1;
-		System.out.println("Player got "+diceRolled);		
+		return diceRolled;
 	}
-
+	
 }
 
