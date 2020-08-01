af970dfe34aa3790e7a47309869197bd2f42c3fb
shashank srivastava <shashank.1696@gmail.com>
2020-08-01 19:56:06 +0530

    REFACTOR: Ensuring that the player position is below 0 then the position should remain 0

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -6,10 +6,23 @@ public class SnakeAndLadder {
 	int START_POSITION = 0;
 	int noOFPLayers = 1;
 	int position = START_POSITION;
+	final int MAX_POSITION = 100;
 
 	public static void main(String[] args) {
 		SnakeAndLadder sl = new SnakeAndLadder();
-		sl.checkOption();
+		System.out.println("Players start position is "+ sl.START_POSITION);
+		sl.game();
+		System.out.println("Players final position is "+ sl.position);
+	}
+	
+	//Here function game is used to run the game till the position reached maximum value   	
+	private void game() {
+		Random random = new Random();
+		while(position != MAX_POSITION) {
+			int diceRolled = this.rollDice();
+			int options = random.nextInt(3); // To get the option where 0-NoPlay 1-Ladder 2-Sanke
+			position = this.checkOption(options, diceRolled);
+		}
 	}
 	
 	 //Here the dice is rolled to get the value in between 1 to 6
@@ -18,35 +31,23 @@ public class SnakeAndLadder {
 		int diceRolled = random.nextInt(6) + 1;
 		return diceRolled;
 	 }
-	
-	//Here the function checkOption is used to get Option
-	 	/*
-	 	 * 0- Noplay
-	 	 * 1- Ladder
-	 	 * 2- Snake
-	 	 */
-	private void checkOption() {
-		
-		int diceRolled = this.rollDice();
-		Random random = new Random();
-		int options = random.nextInt(3);
-		
+
+	//Here the function checkOption is used to return the position of player	
+	private int checkOption(int options, int  diceRolled) {
 		switch(options) {
-		case 0:
-				System.out.println("There is NOPLAY for the player..");
-				break ;		
 		case 1:
-				position = position + diceRolled;
-				break ;
+			position = position + diceRolled;
+			return position;
 		case 2:
-				position = position - diceRolled;
-				if(position < 0) {
-					position = 0;
-					System.out.println(position);
-				} else {
-					System.out.println(position);
-				}
-				break ;
+			position = position - diceRolled;
+			if(position < 0) {
+				position = 0;
+				return position;
+			} else {
+				return position;
+			}	
+		default:
+			return position;	
 		}
 	}
 }
